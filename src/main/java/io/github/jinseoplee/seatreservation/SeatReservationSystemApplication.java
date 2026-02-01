c73a53baa36bf07425259319c7a13583a6612a4d
jinseoplee <jjjp9966@gmail.com>
2026-02-01 16:52:58 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/io/github/jinseoplee/seatreservation/SeatReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.jinseoplee.seatreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jinseoplee/seatreservation/SeatReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jinseoplee.seatreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
